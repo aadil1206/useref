000d5788297aa0888955c1584407aabfbdf79dad
aahiraashrafi <adilmohammadkgn786@gmail.com>
2023-07-09 17:15:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const count = useRef(0);
   const focus=useRef(null)
+  const focus12=useRef(null)
 
   useEffect(() => {
     count.current = count.current + 1;
     focus.current.focus();
   });
-
+ const handlefocus=()=>{
+  focus12.current.style.backgroundColor="red"
+ }
   return (
     <>
       <input
       ref={focus}
+      // onFocus={handlefocus}
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <h1>Render Count: {count.current}</h1>
+      <h1 >Render Count: {count.current}</h1>
+       <input
+      ref={focus12}
+      onFocus={handlefocus}
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
     </>
   );
 }
